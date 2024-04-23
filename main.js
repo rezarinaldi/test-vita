63c27f98a84498ca1d6bbb0b099bfb00f38f1237
rezarinaldi <rez4rinaldi@gmail.com>
2024-04-23 10:13:05 +0700

    feat: adding search for notes item

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const API_ENDPOINT = "https://v1.appbackend.io/v1/rows/768NKI6qq7pq";
 const notesContainer = document.getElementById("noteCard");
 const searchInput = document.getElementById("searchInput");
 const form = document.getElementById("form");
+let dataNotes = [];
 
 form.addEventListener("submit", async (event) => {
   // avoid default behavior
@@ -48,6 +49,7 @@ async function deleteNote(id) {
 async function getAllData() {
   const res = await fetch(API_ENDPOINT);
   const data = await res.json();
+  dataNotes = data;
   return data;
 }
 
@@ -137,3 +139,93 @@ async function buildApp() {
 }
 
 buildApp();
+
+searchInput.addEventListener("keyup", () => {
+  const searchValue = searchInput.value;
+  notesContainer.innerHTML = "";
+
+  const filteredNotes = dataNotes.data.filter((note) =>
+    note.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  filteredNotes.forEach((note) => {
+    const newNoteElement = document.createElement("div");
+    const newNoteTitleElement = document.createElement("h3");
+    const newNoteContentElement = document.createElement("p");
+    const newNoteDateElement = document.createElement("p");
+    const newNoteBtn = document.createElement("a");
+
+    newNoteTitleElement.textContent = note.title;
+    newNoteContentElement.textContent = note.content;
+    newNoteDateElement.textContent = note.date;
+    newNoteBtn.textContent = "See note";
+    newNoteBtn.href = `note.html?id=${note._id}`;
+
+    // EDIT
+    const newNoteEdit = document.createElement("a");
+    newNoteEdit.href = `editNote.html?id=${note._id}`;
+    newNoteEdit.classList.add(
+      "ri-edit-2-line",
+      "absolute",
+      "bottom-3",
+      "right-3",
+      "text-indigo-800"
+    );
+
+    // DELETE
+    const newNoteDelete = document.createElement("button");
+    newNoteDelete.classList.add(
+      "ri-delete-bin-line",
+      "absolute",
+      "top-3",
+      "right-3",
+      "text-indigo-800"
+    );
+    newNoteDelete.addEventListener("click", async () => {
+      await deleteNote(note._id);
+    });
+
+    newNoteTitleElement.classList.add(
+      "text-xl",
+      "font-bold",
+      "text-red-600",
+      "mt-5"
+    );
+    newNoteContentElement.classList.add("text-slate-800");
+    newNoteDateElement.classList.add("text-gray-500", "text-sm", "mt-4");
+    newNoteBtn.classList.add(
+      "bg-yellow-200",
+      "text-slate-800",
+      "p-1",
+      "rounded-lg",
+      "w-[100px]",
+      "text-center",
+      "shadow-md",
+      "hover:bg-slate-800",
+      "hover:text-white"
+    );
+
+    newNoteElement.classList.add(
+      "border",
+      "border-gray-100",
+      "p-4",
+      "bg-white",
+      "shadow-md",
+      "rounded-md",
+      "shadow-black/20",
+      "grid",
+      "grid-cols-1",
+      "relative"
+    );
+
+    newNoteElement.append(
+      newNoteTitleElement,
+      newNoteContentElement,
+      newNoteDateElement,
+      newNoteBtn,
+      newNoteEdit,
+      newNoteDelete
+    );
+    notesContainer.append(newNoteElement);
+  });
+});
